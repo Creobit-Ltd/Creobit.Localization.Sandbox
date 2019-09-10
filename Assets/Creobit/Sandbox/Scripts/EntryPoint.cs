e76826f0e64ea6c44d2a89910d77f56e9db22852
Epishev Maxim <epishev.max@gmail.com>
2019-09-10 16:35:25 +0300

    Add a sandbox scene

---
+++ Assets/Creobit/Sandbox/Scripts/EntryPoint.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using Creobit.Localization;
+
+public class EntryPoint : MonoBehaviour
+{
+    #region MonoBehaviour
+
+    private void Awake()
+    {
+        Debug.Assert(_localizationData != null);
+
+        var key = "text";
+
+        var value = _localizationData.GetString(key, English);
+        Debug.LogFormat("English: {0}", value);
+
+        value = _localizationData.GetString(key, Russian);
+        Debug.LogFormat("Russian: {0}", value);
+
+        value = _localizationData.GetString(key, German);
+        Debug.LogFormat("German: {0}", value);
+
+        _localizationSystem = new LocalizationSystem(_localizationData);
+        _localizationSystem.DefaultLanguage = English;
+
+        Localizer.LocalizationSystem = _localizationSystem;
+
+        _localizationSystem.SetCurrentLanguage(English);
+    }
+
+    #endregion
+    #region EntryPoint
+
+    private readonly string Russian = "ru-RU";
+    private readonly string English = "en-US";
+    private readonly string German = "de-DE";
+
+    [SerializeField]
+    private LocalizationData _localizationData = null;
+
+    [SerializeField]
+    private AudioSource _audioSource = null;
+
+    private ILocalizationSystem _localizationSystem;
+
+    public void OnButtonClick()
+    {
+        var language = _localizationSystem.CurrentLanguage;
+
+        if (language == Russian)
+            language = English;
+        else if (language == English)
+            language = German;
+        else if (language == German)
+            language = Russian;
+
+        _localizationSystem.SetCurrentLanguage(language);
+
+        _audioSource?.Play();
+    }
+
+    #endregion
+}
